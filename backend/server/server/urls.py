ced439a1f2cac00e626f9345348ce0707909bbdb
vaastav <vaastav.anand05@gmail.com>
2017-11-23 01:31:02 -0800

    Add Registration

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from eTone import views as eTone_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
+    url(r'^signup/$', eTone_views.signup, name='signup'),
 ]
