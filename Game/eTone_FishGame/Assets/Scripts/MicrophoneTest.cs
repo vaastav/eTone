0788530d3cdfcba9ed4aec964d5bf2b9f7aff2d5
Jamie Polintan <jamiecpol@gmail.com>
2018-02-27 18:16:32 -0800

    Added some Unity class testing with microphones.

---
+++ Game/eTone_FishGame/Assets/Scripts/MicrophoneTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MicrophoneTest : MonoBehaviour {
+
+    public string[] GetV;
+
+    public string theString;
+
+    public AudioSource aud;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        GetV = Microphone.devices;
+
+        theString = (string) GetV.GetValue(0);
+
+        foreach (string microphone in GetV)
+        {
+            Debug.Log("Microphone is" + microphone.ToString());
+        }
+
+
+        aud = GetComponent<AudioSource>();
+
+	}
+
+    void Update()
+    {
+     
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Recording now.");
+            aud.clip = Microphone.Start("", true, 10, 44100);
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            Debug.Log("End recording.");
+            Microphone.End("");
+        }
+    
+
+
+
+    }
+
+}
