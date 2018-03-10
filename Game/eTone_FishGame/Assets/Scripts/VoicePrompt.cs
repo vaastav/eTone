c8dd53ac2b910b85e0ed8247a73bb4af00369d49
Jamie Polintan <jamiecpol@gmail.com>
2018-03-09 23:31:33 -0800

    Added voiceprompt in the game. Deleted some depreciated scripts (Agent.cs)

---
+++ Game/eTone_FishGame/Assets/Scripts/VoicePrompt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VoicePrompt : MonoBehaviour {
+    /* The voice playback of the default, native speaker's phrase. */
+
+    private AudioSource voice;
+
+    private float timeToPlay = 5;
+
+    private float timePassed = 0;
+
+    private float End = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+        voice = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timePassed += Time.deltaTime;
+       
+
+        if (timePassed >= timeToPlay && End == 0)
+        {
+            
+            timePassed = 0;
+            voice.Play();
+            End++;
+        }
+
+        if (Input.GetKeyDown("p") && timePassed > timeToPlay)
+        {
+            voice.Play();
+        }
+        
+	}
+}
