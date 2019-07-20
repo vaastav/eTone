dc91c231c6d9d4336bf2cb63f8f44a853845d34e
vaastav <vaastav.anand05@gmail.com>
2019-07-20 13:18:45 +0200

    Use LoginView instead of login

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -9,8 +9,8 @@ from eTone import views as eTone_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'template_name': 'logout.html'}, name='logout'),
     url(r'^signup/$', eTone_views.signup, name='signup'),
     url(r'^upload/$', eTone_views.upload_file, name='upload'),
     url(r'^game/$', eTone_views.select_sound_game, name='game'),
