cc4b3653b1069dcacf727a9bfff047f4e28b77fd
Vaastav Anand <vaastav.anand05@gmail.com>
2021-12-31 22:15:24 +0100

    Upgrade app to new django version

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
@@ -7,15 +7,15 @@ from django.views.generic.base import TemplateView
 from eTone import views as eTone_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), {'template_name': 'logout.html'}, name='logout'),
-    url(r'^signup/$', eTone_views.signup, name='signup'),
-    url(r'^upload/$', eTone_views.upload_file, name='upload'),
-    url(r'^game/$', eTone_views.select_sound_game, name='game'),
-    url(r'^stats/$', eTone_views.get_stats, name='stats'),
-    url(r'^api/upload/(?P<typeID>[^/]?)/(?P<filename>[^/]+)$', eTone_views.FileUploadView.as_view())
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), {'template_name': 'logout.html'}, name='logout'),
+    re_path(r'^signup/$', eTone_views.signup, name='signup'),
+    re_path(r'^upload/$', eTone_views.upload_file, name='upload'),
+    re_path(r'^game/$', eTone_views.select_sound_game, name='game'),
+    re_path(r'^stats/$', eTone_views.get_stats, name='stats'),
+    re_path(r'^api/upload/(?P<typeID>[^/]?)/(?P<filename>[^/]+)$', eTone_views.FileUploadView.as_view())
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
